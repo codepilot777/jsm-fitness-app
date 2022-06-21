0f8f53796ecb59b6b60a27aadc82bc41ec3128d7
codepilot777 <codepilot777@gmail.com>
2022-06-21 18:59:35 +0800

    second commit

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,13 +1,33 @@
 import React, { useEffect, useState } from 'react'
 import { Box, Button, Stack, TextField, Typography} from '@mui/material';
 
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+import HorizontalScrollbar from './HorizontalScrollbar';
 
-const SearchExercises = () => {
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart}) => {
 
   const [search, setSearch] = useState('');
+  const [bodyParts, setBodyParts] = useState([]);
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+      
+      setBodyParts(['all', ...bodyPartsData]);
+    }
+    fetchExercisesData();
+  }, [])
+
   const handleSearch = async () => {
     if (search) {
-      // const exercise = await fetchData();
+      const exerciseData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+      const searchedExercise = exerciseData.filter((exercise) => exercise.name.toLowerCase().includes(search.toLowerCase())
+      || exercise.target.toLowerCase().includes(search.toLowerCase())
+      || exercise.equipment.toLowerCase().includes(search.toLowerCase())
+      || exercise.bodyPart.toLowerCase().includes(search.toLowerCase()));
+
+      setSearch('');
+      setExercises(searchedExercise);
     }
   }
   return (
@@ -33,6 +53,9 @@ const SearchExercises = () => {
       onClick={handleSearch}>
         Search
       </Button>
+      </Box>
+      <Box sx={{ position: 'relative', width:'100%', p:'20px'}}>
+        <HorizontalScrollbar data={bodyParts} bodyPart={bodyPart} setBodyPart={setBodyPart}/>
       </Box>
     </Stack>
   )
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,6 +1,16 @@
+export const exerciseOptions = {
+    method: 'GET',
+    url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+    headers: {
+        'X-RapidAPI-Key': '0e3866f6d7msh3016d1fb6b96244p1fd4c9jsne2f142233646',
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+};
+  
+
 export const fetchData = async (url, options) => {
     const response = await fetch(url, options);
     const data = await response.json();
-
+    
     return data;
 }
